d918668b0fcc9e89526399c3a5f24623c89059e1
Dmytro_Bykov <mitprog2@gmail.com>
2023-04-13 15:02:16 +0300

    #1. ActiveMQ: request-reply interaction between two applications using a temporary queue

---
+++ jms-activemq-publish-subscribe/src/main/java/org/mitprog1/jms/interaction/MessageProtocol.java
@@ -0,0 +1,14 @@
+package org.mitprog1.jms.interaction;
+
+public class MessageProtocol {
+    public String handleProtocolMessage(String messageText) {
+        String responseText;
+        if ("MyProtocolMessage".equalsIgnoreCase(messageText)) {
+            responseText = "I recognize your protocol message";
+        } else {
+            responseText = "Unknown protocol message: " + messageText;
+        }
+
+        return responseText;
+    }
+}
